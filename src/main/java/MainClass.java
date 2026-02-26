6d016f51c3dc70ca26158cd9d18fdd700d3cdf0c
Josue-Niyonkunda <josueniyonkunda11@gmail.com>
2026-02-26 11:02:58 +0200

    Adding person names displayer method

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,17 @@
 public class MainClass {
+    String fname;
+    String lname;
+    public MainClass(String F_name,String L_name){
+        fname=F_name;
+        lname=L_name;
+
+    }
+    public void displayer(){
+        System.out.println("My first name is "+ fname+ " and my last name is " +lname.toUpperCase());
+    }
+    public static void  main(String[] args){
+        MainClass person=new MainClass("Josue","Niyonkunda");
+        person.displayer();
+
+    }
 }
