9f0df66ab78090a79f9a4944c707d50fdd21f18d
Josue-Niyonkunda <josueniyonkunda11@gmail.com>
2026-02-27 09:26:00 +0200

    remove commented codes

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -5,21 +5,8 @@ import org.junit.jupiter.api.Test;
 import javax.swing.*;
 import java.awt.*;
 
-public class TestClass extends GuiCalculator {
+public class TestClass {
     static SortFunctionality sortFunctionality;
-    // git actions exercises by simple calculator
-
-
-     @AfterAll
-     public static void testCalculator() {
-        if (!GraphicsEnvironment.isHeadless()) {
-            SwingUtilities.invokeLater(() -> new GuiCalculator().setVisible(true));
-        } else {
-            System.out.println("Running in CI — GUI disabled");
-        }
-
-    }
-
     @BeforeEach
     void setup() {
         sortFunctionality = new SortFunctionality();
